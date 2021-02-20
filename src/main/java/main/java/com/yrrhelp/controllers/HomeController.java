bea542d003268b77d8a1c4bd5a1699a21da68c42
QuocHieuNguyen <50551410+QuocHieuNguyen@users.noreply.github.com>
2021-02-20 20:25:56 +0700

    add login feature

---
+++ src/main/java/main/java/com/yrrhelp/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package main.java.com.yrrhelp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String showHomePage() {
+		
+		System.out.println("In Home Page");
+		
+		return "home";
+	}
+}
